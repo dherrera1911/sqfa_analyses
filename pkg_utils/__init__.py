822851496ed5f1105b78d42b3c05de60fe3bd19f
dherrera1911 <dherrera1911@gmail.com>
2026-03-28 13:23:49 -0400

    Add pca functionality to init

--- pkg_utils/__init__.py
+++ pkg_utils/__init__.py
@@ -1,6 +1,7 @@
 """Shared utility exports used across the paper experiments and analyses."""
 
 from .data import scale_and_center, train_val_split
+from .pca import fit_preprocessing_pca, transform_with_pca, lift_filters_from_pca
 from .evaluation import (
     qda_accuracy,
     qda_accuracy_gaussian,
@@ -8,7 +9,11 @@ from .evaluation import (
     collect_metric_across_runs,
 )
 from .artifacts import artifact_path, has_saved_artifacts, load_cached_filters, save_training_artifacts
-from .regularization import load_or_validate_noise, validate_lfda_k, validate_regularization
+from .regularization import (
+    load_or_validate_noise,
+    validate_lfda_k,
+    validate_regularization,
+)
 from .training import fit_sqfa_adaptive_precision, train_sqfa_repeated
 from .suppca import SupervisedPCA
 from .plot import plot_filter_grid, plot_metric_with_errorbars
@@ -17,6 +22,9 @@ from .plot import plot_filter_grid, plot_metric_with_errorbars
 __all__ = [
     "scale_and_center",
     "train_val_split",
+    "fit_preprocessing_pca",
+    "transform_with_pca",
+    "lift_filters_from_pca",
     "qda_accuracy",
     "qda_accuracy_gaussian",
     "knn_accuracy",
@@ -29,6 +37,7 @@ __all__ = [
     "load_or_validate_noise",
     "fit_sqfa_adaptive_precision",
     "train_sqfa_repeated",
+    "balanced_subset_indices",
     "SupervisedPCA",
     "plot_filter_grid",
     "plot_metric_with_errorbars",
